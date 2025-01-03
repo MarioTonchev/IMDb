2ea8fe04c497808cb56898d1248ea4c3135414cc
MarioTonchev <mariotonchev500@gmail.com>
2025-01-04 00:17:37 +0200

    Added helper functions for the functionality of deleting a movie

--- IMDb/HelperFunctions.cpp
+++ IMDb/HelperFunctions.cpp
@@ -91,6 +91,98 @@ void ReadMoviesFromFile(Movie movies[], int& movieCount) {
 
 	inputFile.close();
 }
+void RemoveMovieFromFile(int index) {
+	ifstream inputFile(MOVIES_FILE);
+	ofstream tempFile(TEMP_MOVIES_FILE);
+
+	if (!inputFile || !tempFile)
+	{
+		cout << "Error opening file." << endl;
+		return;
+	}
+
+	char buffer[MAX_BUFFER_LENGHT];
+	int currLine = 0;
+
+	while (inputFile.getline(buffer, sizeof(buffer)))
+	{
+		if (currLine != index)
+		{
+			tempFile << buffer << endl;
+		}
+
+		currLine++;
+	}
+
+	inputFile.close();
+	tempFile.close();
+
+	remove(MOVIES_FILE);
+	rename(TEMP_MOVIES_FILE, MOVIES_FILE);
+}
+
+void DeleteMovieFromArray(Movie movies[], int& movieCount, int index) {
+	for (size_t i = index; i < movieCount; i++)
+	{
+		Movie nextMovie = movies[i + 1];
+
+		movies[i] = nextMovie;
+	}
+
+	movieCount--;
+}
+int FindMovieByTitle(char* title, Movie movies[], int movieCount) {
+	for (size_t i = 0; i < movieCount; i++)
+	{
+		char* movieTitleLowered = ConvertWordToLower(movies[i].title);
+
+		if (strcmp(movieTitleLowered, title) == 0)
+		{
+			delete[] movieTitleLowered;
+			return i;
+		}
+
+		delete[] movieTitleLowered;
+	}
+
+	return -1;
+}
+
+bool IsLetter(char symbol) {
+	if ((symbol >= 'a' && symbol <= 'z') || (symbol >= 'A' && symbol <= 'Z'))
+	{
+		return true;
+	}
+
+	return false;
+}
+char* ConvertWordToLower(char* word) {
+	const int LETTER_DIFFERENCE = 'a' - 'A';
+
+	char* result = new char[MAX_GENRE_LENGTH];
+	int resultCnt = 0;
+
+	char* wordCopy = word;
+
+	while (*wordCopy != '\0')
+	{
+		if (IsLetter(*wordCopy) && (*wordCopy >= 'A' && *wordCopy <= 'Z'))
+		{
+			result[resultCnt] = *wordCopy + LETTER_DIFFERENCE;
+		}
+		else
+		{
+			result[resultCnt] = *wordCopy;
+		}
+
+		resultCnt++;
+		wordCopy++;
+	}
+
+	result[resultCnt] = '\0';
+
+	return result;
+}
 
 void PressAnyKeyToContinue() {
 	cout << endl;
--- IMDb/HelperFunctions.h
+++ IMDb/HelperFunctions.h
@@ -6,5 +6,12 @@ void ClearInputBuffer();
 
 void SaveMovieToFile(Movie movie, const char* fileName);
 void ReadMoviesFromFile(Movie movies[], int& movieCount);
+void RemoveMovieFromFile(int index);
+
+void DeleteMovieFromArray(Movie movies[], int& movieCount, int index);
+int FindMovieByTitle(char* title, Movie movies[], int movieCount);
+
+bool IsLetter(char symbol);
+char* ConvertWordToLower(char* word);
 
 void PressAnyKeyToContinue();

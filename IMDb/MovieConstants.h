666a703f12e44116a80be1c83d95384a8184c1a3
MarioTonchev <mariotonchev500@gmail.com>
2025-01-03 23:55:50 +0200

    Added constant variables

---
+++ IMDb/MovieConstants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+const int MAX_TITLE_LENGTH = 100;
+const int MAX_GENRE_LENGTH = 50;
+const int MAX_DIRECTOR_LENGTH = 50;
+const int MAX_ACTORS_LENGTH = 300;
+const int MAX_MOVIES = 20;
+
+static const char* MOVIES_FILE = "Movies.txt";
+static const char* TEMP_MOVIES_FILE = "Temp.txt";
+const int MAX_BUFFER_LENGHT = 550;
